67dec7b26b43ba81ae6dab6e8c193f1081a21192
Madhumitha <mithalns@gmail.com>
2020-09-09 14:25:52 +0200

    changes made

--- num.js
+++ num.js
@@ -2,7 +2,10 @@ function minAndMaxInArray(array) {
   array.sort();
   let minOfArray = array[0];
   let maxOfArray = array[array.length - 1];
-  console.log(`Minimum of Array ${minOfArray}, Maximum of Array ${maxOfArray}`);
+
+  return { minOfArray, maxOfArray };
 }
 
-minAndMaxInArray([8, 9, 5, 5]);
+let minAndMax = minAndMaxInArray([3, 6, 8, 9]);
+
+console.log(minAndMax);
